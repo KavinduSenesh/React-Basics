3ab7bd3be046047d92ef14faa2b6a770fa0b1b14
Kavindu Senesh <ksenesh5@gmail.com>
2025-02-10 09:34:49 +0530

    setup basic routes

--- react-native-basics/App.tsx
+++ react-native-basics/App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import tw from "twrnc";
+import { View } from 'react-native';
 
 export default function App() {
-  return (
-      <View style={tw`flex-1 bg-white justify-center items-center`}>
-        <Text style={tw`text-blue-500 text-lg`}>Shut up CSS!</Text>
-        <StatusBar style="auto" />
-      </View>
-  );
+    return (<View></View>)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
